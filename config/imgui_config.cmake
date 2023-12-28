4e2f0663c26bae17cf00c536b459c50befc0b895
X-ray <61073708+X-rays5@users.noreply.github.com>
2023-12-28 15:19:04 +0100

    Fix config only being applied during build

--- config/imgui_config.cmake
+++ config/imgui_config.cmake
@@ -11,31 +11,31 @@ option(IMGUI_ENABLE_FREETYPE_LUNASVG "Enable LunaSVG" ON)
 option(IMGUI_DEBUG_PARANOID "Enable slower asserts" OFF)
 
 if (IMGUI_DISABLE_OBSOLETE_FUNCTIONS)
-    target_compile_definitions(${PROJECT_NAME} PRIVATE IMGUI_DISABLE_OBSOLETE_FUNCTIONS)
+    target_compile_definitions(${PROJECT_NAME} PUBLIC IMGUI_DISABLE_OBSOLETE_FUNCTIONS)
 endif()
 
 if (IMGUI_DISABLE_OBSOLETE_KEYIO)
-    target_compile_definitions(${PROJECT_NAME} PRIVATE IMGUI_DISABLE_OBSOLETE_KEYIO)
+    target_compile_definitions(${PROJECT_NAME} PUBLIC IMGUI_DISABLE_OBSOLETE_KEYIO)
 endif()
 
 if (IMGUI_DISABLE_DEMO_WINDOWS)
-    target_compile_definitions(${PROJECT_NAME} PRIVATE IMGUI_DISABLE_DEMO_WINDOWS)
+    target_compile_definitions(${PROJECT_NAME} PUBLIC IMGUI_DISABLE_DEMO_WINDOWS)
 endif()
 
 if (IMGUI_DISABLE_DEBUG_TOOLS)
-    target_compile_definitions(${PROJECT_NAME} PRIVATE IMGUI_DISABLE_DEBUG_TOOLS)
+    target_compile_definitions(${PROJECT_NAME} PUBLIC IMGUI_DISABLE_DEBUG_TOOLS)
 endif()
 
 if (IMGUI_DISABLE_WIN32_FUNCTIONS)
-    target_compile_definitions(${PROJECT_NAME} PRIVATE IMGUI_DISABLE_WIN32_FUNCTIONS)
+    target_compile_definitions(${PROJECT_NAME} PUBLIC IMGUI_DISABLE_WIN32_FUNCTIONS)
 endif()
 
 if (IMGUI_ENABLE_FREETYPE_LUNASVG)
     CPMAddPackage("gh:sammycage/lunasvg#9a05e196382ebe4ef7993039d069ab6312b60fac")
     target_link_libraries(${PROJECT_NAME} PRIVATE lunasvg)
-    target_compile_definitions(${PROJECT_NAME} PRIVATE IMGUI_ENABLE_FREETYPE_LUNASVG)
+    target_compile_definitions(${PROJECT_NAME} PUBLIC IMGUI_ENABLE_FREETYPE_LUNASVG)
 endif()
 
 if (IMGUI_DEBUG_PARANOID)
-    target_compile_definitions(${PROJECT_NAME} PRIVATE IMGUI_DEBUG_PARANOID)
+    target_compile_definitions(${PROJECT_NAME} PUBLIC IMGUI_DEBUG_PARANOID)
 endif()
